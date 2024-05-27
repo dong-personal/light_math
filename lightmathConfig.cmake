d177c62aa757c39d7b95f575c7bba67b662c8503
wd <dluck.w@qq.com>
2024-05-27 19:43:16 +0800

    cmakefile

---
+++ lightmathConfig.cmake
@@ -0,0 +1,6 @@
+
+# 创建 INTERFACE 库
+add_library(lightmath INTERFACE)
+
+# 设置这个库的包含目录
+set_target_properties(lightmath PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "/home/dong/desktop/light_math/lightmath")
